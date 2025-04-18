bd5ea699155a371caa27bbf979b39d985e63d9a6
Venkatesh M <venkatesh2737@gmail.com>
2025-04-18 09:20:38 +0530

    update with unittest framework

--- src/main/filter_nulls.py
+++ src/main/filter_nulls.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-def filter_invalid_rows(input_path, output_path):
+def filter_invalid_rows(input_path, output_path,test_run=False):
     # Initialize SparkSession
     spark = SparkSession.builder \
         .appName("Filter Invalid Rows") \
@@ -14,7 +14,8 @@ def filter_invalid_rows(input_path, output_path):
     valid_data_df = data_df.filter(col("id").isNotNull() & col("name").isNotNull())
 
     # Write the valid rows to the output path
-    valid_data_df.write.csv(output_path, header=True)
+    valid_data_df.write.mode("overwrite").csv(output_path, header=True)
 
-    # Stop SparkSession
-    spark.stop()
+    if not test_run:
+        # Stop SparkSession
+        spark.stop()
