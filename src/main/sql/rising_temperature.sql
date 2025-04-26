2e121a1f0cc00a0351daca58cf3003c8f97025ca
Venkatesh M <venkatesh2737@gmail.com>
2025-04-26 14:32:09 +0530

    add leetcode sql exercises with solutions

---
+++ src/main/sql/rising_temperature.sql
@@ -0,0 +1,51 @@
+-- Table: Weather
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the column with unique values for this table.
+-- There are no different rows with the same recordDate.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+
+-- Explanation: 
+-- In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+-- In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+-- Solution MS SQL:
+select ID from (select *
+,lag(temperature ) over(partition by 1 order by recordDate) prev
+,lag(recordDate ) over(partition by 1 order by recordDate) prevDate
+ from weather
+ ) t where temperature > coalesce(prev,temperature) and dateadd(d,-1,recorddate) = prevDate
